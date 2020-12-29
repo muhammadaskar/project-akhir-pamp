a198339819869e8b0f9b084c81478f9538308a29
Muhammad Askar <muhammadaskar74@gmail.com>
2020-12-29 22:53:21 +0800

    update ip

--- lib/helper/HttpService.dart
+++ lib/helper/HttpService.dart
@@ -4,11 +4,11 @@ import 'package:http/http.dart';
 import 'package:projek_akhir/model/news_model.dart';
 
 class HttpService {
-  final String url = "http://127.0.0.1/ci-simple-api/index.php/api/news";
+  final String url = "http://192.168.1.11/ci-simple-api/index.php/api/news";
+  Client client = Client();
 
   Future<List<News>> getNews() async {
     Response res = await get(url);
-
     if (res.statusCode == 200) {
       List<dynamic> body = jsonDecode(res.body);
       List<News> news =
--- lib/model/news_model.dart
+++ lib/model/news_model.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
-class News{
+class News {
   final int id_news;
   final String title;
   final String description;
@@ -8,23 +8,21 @@ class News{
   final String pubdate;
   final String creator;
 
-  News({
-      @required this.id_news,
+  News(
+      {@required this.id_news,
       @required this.title,
       @required this.description,
       @required this.link,
       @required this.pubdate,
       @required this.creator});
 
-
-  factory News.FromJson(Map<String, dynamic> json){
+  factory News.FromJson(Map<String, dynamic> json) {
     return News(
-        id_news: json['id_news'] as int,
-      title: json['title'] as String,
-      description: json['description'] as String,
-      link : json['json'] as String,
-      pubdate: json['pubdate'] as String,
-      creator: json['creator'] as String
-    );
+        // id_news: json['id_news'] as int,
+        title: json['title'] as String,
+        description: json['description'] as String,
+        link: json['json'] as String,
+        pubdate: json['pubdate'] as String,
+        creator: json['creator'] as String);
   }
 }
